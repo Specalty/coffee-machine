ab3d690de0ea4e4579066c197f2addfeadb31f5c
specalty <sesyomin@ya.ru>
2020-06-18 09:31:23 +0300

    Полная статика

--- index.php
+++ index.php
@@ -47,7 +47,17 @@
               </div>
             </div>
             <div class="col-6"><!--баланс,АТМ и сдача-->
-              
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Баланс">
+                <div class="input-group-append">
+                  <span class="input-group-text">&#8381;</span>
+                </div>
+              </div>
+              <div class="atm">
+                <img src="img/bill_acc.png" alt="">
+              </div>
+              <button class="btn btn-primary btn-block mt-3 change-btn">Сдача</button>
+              <div class="change-box mt-3"></div>
             </div>
           </div>
         </div>
